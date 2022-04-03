4c79fa6a4c81fa004ff095753c0ba17158dac988
floppyhammer <tannhauser_chen@outlook.com>
2022-04-03 09:32:16 +0800

    Remove ECS

--- demo/native/main.cpp
+++ demo/native/main.cpp
@@ -6,11 +6,6 @@
 #include "../../src/common/global_macros.h"
 #include "../common/app.h"
 
-#include "../../src/modules/vgui/ecs/coordinator.h"
-#include "../../src/modules/vgui/ecs/components/components.h"
-#include "../../src/modules/vgui/ecs/systems/physics_system.h"
-#include "../../src/modules/vgui/ecs/systems/render_system.h"
-
 #include <iostream>
 
 using namespace Pathfinder;
@@ -47,59 +42,9 @@ int main() {
 
     App app(window.width, window.height, area_lut_input, font_input, svg_input);
 
-    // ECS test.
-    // ----------------------------------------------------------
-    auto coordinator = Coordinator::get_singleton();
-
-    coordinator.register_component<Gravity>();
-    coordinator.register_component<RigidBody>();
-    coordinator.register_component<Transform>();
-
-    auto physics_system = coordinator.register_system<PhysicsSystem>();
-    // Set signature for physics system.
-    {
-        Signature signature;
-        signature.set(coordinator.get_component_type<Gravity>());
-        signature.set(coordinator.get_component_type<RigidBody>());
-        signature.set(coordinator.get_component_type<Transform>());
-        coordinator.set_system_signature<PhysicsSystem>(signature);
-    }
-
-    auto render_system = coordinator.register_system<RenderSystem>();
-
-    // Allocate space for entities.
-    std::vector<Entity> entities(10);
-
-    // Create some entities.
-    for (auto &entity: entities) {
-        entity = coordinator.create_entity();
-
-        coordinator.add_component(
-                entity,
-                Gravity{Vec3<float>(0.0f, 0.0f, 0.0f)});
-
-        coordinator.add_component(
-                entity,
-                RigidBody{
-                        Vec3<float>(0.0f, 0.0f, 0.0f),
-                        Vec3<float>(0.0f, 0.0f, 0.0f),
-                });
-
-        coordinator.add_component(
-                entity,
-                Transform{
-                        Vec3<float>(0.0f, 0.0f, 0.0f),
-                        Quaternion{0.0f, 0.0f, 0.0f, 0.0f},
-                        Vec3<float>(0.0f, 0.0f, 0.0f),
-                });
-    }
-    // ----------------------------------------------------------
-
     // Rendering loop.
     while (!glfwWindowShouldClose(window.get_glfw_window())) {
         window.handle_inputs();
-
-        physics_system->update(0.001f);
 
         app.loop();
 
--- src/modules/vgui/ecs/component.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by floppyhammer on 4/2/2022.
-//
-
-#include "component.h"
