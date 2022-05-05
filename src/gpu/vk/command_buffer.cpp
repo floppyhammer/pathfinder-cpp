9d10bd2d82d713ad82a032501c2c1a1a812e0a30
floppyhammer <tannhauser_chen@outlook.com>
2022-05-05 14:50:39 +0800

    Add a note to buffer mapping

--- src/gpu/vk/command_buffer.cpp
+++ src/gpu/vk/command_buffer.cpp
@@ -140,15 +140,22 @@ namespace Pathfinder {
             Logger::error("Tried to upload invalid data to buffer!");
         }
 
+        // FIXME: This does not work.
         // Update buffer by memory mapping.
-        if (buffer->get_usage() == BufferUsage::HOST_VISIBLE_AND_COHERENT) {
-            auto buffer_vk = static_cast<BufferVk *>(buffer.get());
-
-            void *mapping_data;
-            vkMapMemory(vk_device, buffer_vk->get_vk_device_memory(), 0, data_size, 0, &mapping_data);
-            memcpy(mapping_data, &data, data_size);
-            vkUnmapMemory(vk_device, buffer_vk->get_vk_device_memory());
-        }
+//        if (buffer->get_usage() == BufferUsage::HOST_VISIBLE_AND_COHERENT) {
+//            auto buffer_vk = static_cast<BufferVk *>(buffer.get());
+//
+//            void *mapped_data;
+//            auto res = vkMapMemory(vk_device, buffer_vk->get_vk_device_memory(), offset, data_size, 0, &mapped_data);
+//            memcpy(mapped_data, &data, data_size);
+//            vkUnmapMemory(vk_device, buffer_vk->get_vk_device_memory());
+//
+//            if (res != VK_SUCCESS) {
+//                Logger::error("Failed to map memory!", "Vulkan");
+//            }
+//
+//            return;
+//        }
 
         Command cmd;
         cmd.type = CommandType::UploadToBuffer;
