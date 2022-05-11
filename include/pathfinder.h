bdb8c888063ef4ef8400ca0984cb4595c610a492
floppyhammer <tannhauser_chen@outlook.com>
2022-05-11 19:47:40 +0800

    Rename modules folder to core

--- include/pathfinder.h
+++ include/pathfinder.h
@@ -6,7 +6,7 @@
 #include "../src/gpu/vk/texture.h"
 #include "../src/gpu/framebuffer.h"
 #include "../src/gpu/swap_chain.h"
-#include "../src/modules/d3dx/canvas.h"
+#include "../src/core/d3dx/canvas.h"
 #include "../src/common/global_macros.h"
 #include "../src/common/io.h"
 
