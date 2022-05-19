e77c928d15df24104635252c738fbb3efc7a5f22
floppyhammer <tannhauser_chen@outlook.com>
2022-05-19 10:47:35 +0800

    Clean up

--- src/shaders/compile.ps1
+++ src/shaders/compile.ps1
@@ -1,10 +1,12 @@
-# C:/VulkanSDK/1.3.204.0/Bin/glslc.exe blit.vert -o blit_vert.spv
-# C:/VulkanSDK/1.3.204.0/Bin/glslc.exe blit.frag -o blit_frag.spv
+New-Variable -Name "GLSLC" -Visibility Public -Value "C:/VulkanSDK/1.3.204.0/Bin/glslc.exe"
 
-C:/VulkanSDK/1.3.204.0/Bin/glslc.exe d3d9/fill.vert -o d3d9/fill_vert.spv
-C:/VulkanSDK/1.3.204.0/Bin/glslc.exe d3d9/fill.frag -o d3d9/fill_frag.spv
-C:/VulkanSDK/1.3.204.0/Bin/glslc.exe d3d9/tile.vert -o d3d9/tile_vert.spv
-C:/VulkanSDK/1.3.204.0/Bin/glslc.exe d3d9/tile.frag -o d3d9/tile_frag.spv
+& $GLSLC blit.vert -o blit_vert.spv
+& $GLSLC blit.frag -o blit_frag.spv
+
+& $GLSLC d3d9/fill.vert -o d3d9/fill_vert.spv
+& $GLSLC d3d9/fill.frag -o d3d9/fill_frag.spv
+& $GLSLC d3d9/tile.vert -o d3d9/tile_vert.spv
+& $GLSLC d3d9/tile.frag -o d3d9/tile_frag.spv
 
 # Wait for input.
 Write-Host "All jobs finished."
