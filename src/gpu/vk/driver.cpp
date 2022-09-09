31677b9c5dda170e0114fcd5500875f67ee55919
floppyhammer <tannhauser_chen@outlook.com>
2022-09-09 14:50:53 +0800

    Vulkan D3D11 backend now works (with validation errors)

--- src/gpu/vk/driver.cpp
+++ src/gpu/vk/driver.cpp
@@ -395,12 +395,13 @@ namespace Pathfinder {
                 format
         );
 
+        // TODO(floppyhammer): Add VK_IMAGE_USAGE_STORAGE_BIT only when necessary.
         createVkImage(width,
                       height,
                       to_vk_texture_format(format),
                       VK_IMAGE_TILING_OPTIMAL,
                       VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT |
-                      VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+                      VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_STORAGE_BIT,
                       VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
                       texture_vk->image,
                       texture_vk->image_memory);
