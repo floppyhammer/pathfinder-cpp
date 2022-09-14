28496551cd1e8fb482baaeedb7f0b6e515705c92
floppyhammer <tannhauser_chen@outlook.com>
2022-09-14 23:09:47 +0800

    Remove shader minifier script

--- src/shaders/minify.ps1
+++
@@ -1,2 +0,0 @@
-python minifier.py d3d9/fill.vert d3d9/fill.frag d3d9/tile.vert d3d9/tile.frag d3d11/bin.comp d3d11/bound.comp d3d11/dice.comp d3d11/fill.comp d3d11/propagate.comp d3d11/sort.comp d3d11/tile.comp
-pause
