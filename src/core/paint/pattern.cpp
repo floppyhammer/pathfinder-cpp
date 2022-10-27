9187e808277f25771325a8eff320950b2ad26559
floppyhammer <tannhauser_chen@outlook.com>
2022-10-27 15:13:37 +0800

    Fix OR bit operation

--- src/core/paint/pattern.cpp
+++ src/core/paint/pattern.cpp
@@ -1,6 +1,7 @@
 #include "pattern.h"
 
 namespace Pathfinder {
+
 bool Pattern::repeat_x() const {
     return (flags.value & PatternFlags::REPEAT_X) != 0x0;
 }
@@ -9,7 +10,7 @@ void Pattern::set_repeat_x(bool repeat_x) {
     if (repeat_x) {
         flags.value |= PatternFlags::REPEAT_X;
     } else {
-        flags.value &= !PatternFlags::REPEAT_X;
+        flags.value &= ~PatternFlags::REPEAT_X;
     }
 }
 
@@ -21,7 +22,7 @@ void Pattern::set_repeat_y(bool repeat_y) {
     if (repeat_y) {
         flags.value |= PatternFlags::REPEAT_Y;
     } else {
-        flags.value &= !PatternFlags::REPEAT_Y;
+        flags.value &= ~PatternFlags::REPEAT_Y;
     }
 }
 
@@ -33,7 +34,8 @@ void Pattern::set_smoothing_enabled(bool enable) {
     if (!enable) {
         flags.value |= PatternFlags::NO_SMOOTHING;
     } else {
-        flags.value &= !PatternFlags::NO_SMOOTHING;
+        flags.value &= ~PatternFlags::NO_SMOOTHING;
     }
 }
+
 } // namespace Pathfinder
