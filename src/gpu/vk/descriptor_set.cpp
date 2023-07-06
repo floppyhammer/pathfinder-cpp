cd3c2cb9b7e038620440a5a127dda136ffba3824
floppyhammer <tannhauser_chen@outlook.com>
2023-07-06 16:26:26 +0800

    Tidy Vulkan descriptor set

--- src/gpu/vk/descriptor_set.cpp
+++ src/gpu/vk/descriptor_set.cpp
@@ -63,8 +63,6 @@ void DescriptorSetVk::update_vk_descriptor_set(VkDevice _device, VkDescriptorSet
         descriptor_set_allocated = true;
     }
 
-    std::vector<VkWriteDescriptorSet> descriptor_writes;
-
     for (auto &pair : descriptors) {
         auto &descriptor = pair.second;
 
@@ -73,8 +71,8 @@ void DescriptorSetVk::update_vk_descriptor_set(VkDevice _device, VkDescriptorSet
 
         // These two have to be here to prevent them from going out of scope
         // before being used in vkUpdateDescriptorSets().
-        VkDescriptorBufferInfo bufferInfo{};
-        VkDescriptorImageInfo imageInfo{};
+        VkDescriptorBufferInfo buffer_info{};
+        VkDescriptorImageInfo image_info{};
 
         descriptor_write.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
         descriptor_write.dstSet = descriptor_set;
@@ -82,80 +80,60 @@ void DescriptorSetVk::update_vk_descriptor_set(VkDevice _device, VkDescriptorSet
         descriptor_write.dstArrayElement = 0;
         descriptor_write.descriptorCount = 1;
 
-        if (descriptor.type == DescriptorType::UniformBuffer) {
-            // It's possible to bind nothing at a slot in the layout.
-            // However, you may see validation errors.
-            if (descriptor.buffer == nullptr) {
-                continue;
-            }
+        switch (descriptor.type) {
+            case DescriptorType::UniformBuffer:
+            case DescriptorType::StorageBuffer: {
+                // It's possible to bind nothing at a slot in the layout.
+                // However, you may see validation errors.
+                if (descriptor.buffer == nullptr) {
+                    continue;
+                }
 
-            auto buffer_vk = static_cast<BufferVk *>(descriptor.buffer.get());
+                auto buffer_vk = static_cast<BufferVk *>(descriptor.buffer.get());
 
-            bufferInfo.buffer = buffer_vk->get_vk_buffer();
-            bufferInfo.offset = 0;
-            bufferInfo.range = buffer_vk->get_size();
+                buffer_info.buffer = buffer_vk->get_vk_buffer();
+                buffer_info.offset = 0;
+                buffer_info.range = buffer_vk->get_size();
 
-            descriptor_write.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-            descriptor_write.pBufferInfo = &bufferInfo;
-        } else if (descriptor.type == DescriptorType::Sampler) {
-            // It's possible to bind nothing at a slot in the layout.
-            // However, you may see validation errors.
-            if (descriptor.texture == nullptr) {
-                continue;
-            }
+                if (descriptor.type == DescriptorType::UniformBuffer) {
+                    descriptor_write.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+                } else if (descriptor.type == DescriptorType::StorageBuffer) {
+                    descriptor_write.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+                }
 
-            auto texture_vk = static_cast<TextureVk *>(descriptor.texture.get());
+                descriptor_write.pBufferInfo = &buffer_info;
+            } break;
+            case DescriptorType::Sampler:
+            case DescriptorType::Image: {
+                // It's possible to bind nothing at a slot in the layout.
+                // However, you may see validation errors.
+                if (descriptor.texture == nullptr) {
+                    continue;
+                }
 
-            imageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-            imageInfo.imageView = texture_vk->get_image_view();
-            imageInfo.sampler = texture_vk->get_sampler();
+                auto texture_vk = static_cast<TextureVk *>(descriptor.texture.get());
 
-            descriptor_write.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-            descriptor_write.pImageInfo = &imageInfo;
-        } else if (descriptor.type == DescriptorType::StorageBuffer) {
-            // It's possible to bind nothing at a slot in the layout.
-            // However, you may see validation errors.
-            if (descriptor.buffer == nullptr) {
-                continue;
-            }
+                image_info.imageView = texture_vk->get_image_view();
+                image_info.sampler = texture_vk->get_sampler();
 
-            auto buffer_vk = static_cast<BufferVk *>(descriptor.buffer.get());
+                if (descriptor.type == DescriptorType::Sampler) {
+                    image_info.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+                    descriptor_write.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+                } else if (descriptor.type == DescriptorType::Image) {
+                    image_info.imageLayout = VK_IMAGE_LAYOUT_GENERAL;
+                    descriptor_write.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
+                }
 
-            bufferInfo.buffer = buffer_vk->get_vk_buffer();
-            bufferInfo.offset = 0;
-            bufferInfo.range = buffer_vk->get_size();
-
-            descriptor_write.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
-            descriptor_write.pBufferInfo = &bufferInfo;
-        } else if (descriptor.type == DescriptorType::Image) {
-            // It's possible to bind nothing at a slot in the layout.
-            // However, you may see validation errors.
-            if (descriptor.texture == nullptr) {
-                continue;
-            }
-
-            auto texture_vk = static_cast<TextureVk *>(descriptor.texture.get());
-
-            imageInfo.imageLayout = VK_IMAGE_LAYOUT_GENERAL;
-            imageInfo.imageView = texture_vk->get_image_view();
-            imageInfo.sampler = texture_vk->get_sampler();
-
-            descriptor_write.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
-            descriptor_write.pImageInfo = &imageInfo;
-        } else {
-            abort();
+                descriptor_write.pImageInfo = &image_info;
+            } break;
+            default:
+                abort();
         }
 
+        // If you want to update multiple descriptor sets at once,
+        // make sure the pBufferInfo or pImageInfo pointer is still valid at this point.
         vkUpdateDescriptorSets(device, 1, &descriptor_write, 0, nullptr);
     }
-
-    // This doesn't work due to using of pBufferInfo and pImageInfo pointers.
-    //        // Update the contents of a descriptor set object.
-    //        vkUpdateDescriptorSets(device,
-    //                               descriptor_writes.size(),
-    //                               descriptor_writes.data(),
-    //                               0,
-    //                               nullptr);
 }
 
 VkDescriptorSet &DescriptorSetVk::get_vk_descriptor_set() {
