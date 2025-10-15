4b59bf204ec92d4e0bcd0e2eef9c1687a6deb037
floppyhammer <tannhauser_chen@outlook.com>
2025-10-15 16:45:00 +0800

    Fix FenceGl wait

--- pathfinder/gpu/gl/fence.cpp
+++ pathfinder/gpu/gl/fence.cpp
@@ -9,14 +9,8 @@ FenceGl::~FenceGl() {
 }
 
 void FenceGl::wait() const {
-    // Wait indefinitely
-    while (true) {
-        GLenum wait_result = glClientWaitSync(fence, GL_SYNC_FLUSH_COMMANDS_BIT, GL_TIMEOUT_IGNORED);
-
-        if (wait_result == GL_ALREADY_SIGNALED || wait_result == GL_CONDITION_SATISFIED) {
-            return;
-        }
-    }
+    GLenum wait_result = glClientWaitSync(fence, GL_SYNC_FLUSH_COMMANDS_BIT, GL_TIMEOUT_IGNORED);
+    assert(wait_result == GL_ALREADY_SIGNALED || wait_result == GL_CONDITION_SATISFIED);
 }
 
 } // namespace Pathfinder
