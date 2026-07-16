0f628a606a48a60410070a02a47ee1d6fd0b9290
floppyhammer <tannhauser_chen@outlook.com>
2026-07-16 22:18:24 +0800

    Fix dangling pointer reference

--- pathfinder/gpu/mtl/queue.mm
+++ pathfinder/gpu/mtl/queue.mm
@@ -12,8 +12,12 @@ void QueueMtl::submit(const std::shared_ptr<CommandEncoder>& encoder, const std:
 
         auto mtl_cmd_buffer = encoder_mtl->get_handle();
 
+        // We must create a local shared_ptr copy to ensure the block captures the object by value,
+        // rather than capturing the reference to the parameter which will go out of scope.
+        auto encoder_captured = encoder;
+
         [mtl_cmd_buffer addCompletedHandler:^(id<MTLCommandBuffer> cb) {
-            encoder->invoke_callbacks();
+            encoder_captured->invoke_callbacks();
         }];
 
         [mtl_cmd_buffer commit];
